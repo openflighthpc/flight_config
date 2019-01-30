e9661b05a42ee58253702c2c8bb2749bae5b2c9b
william <william.mccumstie@outlook.com>
2019-01-30 11:00:29 +0000

    Require the patched version of TTYConfig
    TTY::Config uses `safe_load` to load `YAML` files, which doesn't allow
    aliases by default. However it is possible to get `TTY::Config` to write
    a `YAML` file with aliases.
    
    To prevent `TTY::Config` breaking in this manner, it has patched to
    always `safe_load` allowing aliases.

--- lib/flight_config/patches/tty_config.rb
+++ lib/flight_config/patches/tty_config.rb
@@ -26,38 +26,38 @@
 # SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #
 
-lib = File.expand_path("../lib", __FILE__)
-$LOAD_PATH.unshift(lib) unless $LOAD_PATH.include?(lib)
-require "flight_config/version"
+require 'tty/config'
+require 'yaml'
 
-Gem::Specification.new do |spec|
-  spec.name          = "flight_config"
-  spec.version       = FlightConfig::VERSION
-  spec.authors       = ["Steve Norledge", "Alces Flight Ltd."]
-  spec.email         = ["dev@alces-software.com"]
+module FlightConfig
+  module Patches
+    module TTYConfig
+      ##
+      # Redefine TTY::Config to use the custom YAML parser
+      #
+      def self.included(base)
+        base.const_set('YAML', PatchedYAML)
+      end
 
-  spec.summary       = "Manages the loading of config files"
-  spec.homepage      = "https://github.com/alces-software/flight_config"
+      module PatchedYAML
+        class << self
+          delegate_missing_to Psych
+        end
 
-  # Prevent pushing this gem to RubyGems.org. To allow pushes either set the 'allowed_push_host'
-  # to allow pushing to a single host or delete this section to allow pushing to any host.
-  if spec.respond_to?(:metadata)
-    spec.metadata["allowed_push_host"] = "TODO: Set to 'http://mygemserver.com'"
-  else
-    raise "RubyGems 2.0 or newer is required to protect against " \
-      "public gem pushes."
+        ##
+        # Overload `safe_load` to always allow aliases
+        #
+        def self.safe_load(yaml,
+                           whitelist_classes = [],
+                           whitelist_symbols = [],
+                           _aliases = false,
+                           filename = nil)
+          super(yaml, whitelist_classes, whitelist_symbols, true, filename)
+        end
+      end
+    end
+
+    TTY::Config.include TTYConfig
   end
-
-  spec.files         = Dir.chdir(File.expand_path('..', __FILE__)) do
-    `git ls-files -z`.split("\x0").reject { |f| f.match(%r{^(test|spec|features)/}) }
-  end
-  spec.bindir        = "exe"
-  spec.executables   = spec.files.grep(%r{^exe/}) { |f| File.basename(f) }
-  spec.require_paths = ["lib"]
-
-  spec.add_runtime_dependency 'tty-config'
-
-  spec.add_development_dependency "bundler", "~> 1.16"
-  spec.add_development_dependency "rake", "~> 10.0"
-  spec.add_development_dependency "rspec", "~> 3.0"
 end
+
