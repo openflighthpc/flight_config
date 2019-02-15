a830ec6165bba5e8322e1569009522e5e6e4cab3
william <william.mccumstie@outlook.com>
2019-02-15 11:46:42 +0000

    Add the basic files for a specification

--- spec/flight_config/core_spec.rb
+++ spec/flight_config/core_spec.rb
@@ -26,23 +26,19 @@
 # SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #
 
-require "bundler/setup"
-require "flight_config"
+require 'flight_config/core'
 
-RSpec.configure do |config|
-  # Enable flags like --only-failures and --next-failure
-  config.example_status_persistence_file_path = ".rspec_status"
+RSpec.describe FlightConfig::Core do
+  subject do
+    Class.new do
+      include described_class
 
-  # Disable RSpec exposing methods globally on `Module` and `main`
-  config.disable_monkey_patching!
+      def path
+        '/tmp/test-path'
+      end
+    end
+  end
 
-  # Run specs in random order to surface order dependencies. If you find an
-  # order dependency and want to debug it, you can fix the order by providing
-  # the seed, which is printed after each run.
-  #     --seed 1234
-  config.order = :random
-
-  config.expect_with :rspec do |c|
-    c.syntax = :expect
+  describe '::lock' do
   end
 end
