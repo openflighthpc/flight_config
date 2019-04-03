a4cbb8fa74f99e6c08d95da460029169920d7532
Mark J. Titorenko <mark.titorenko@alces-flight.com>
2019-04-03 18:45:04 +0100

    Bump dev version

--- lib/flight_config/version.rb
+++ lib/flight_config/version.rb
@@ -25,5 +25,5 @@
 # https://github.com/openflighthpc/flight_config
 #==============================================================================
 module FlightConfig
-  VERSION = "0.1.0"
+  VERSION = "0.2.0-dev"
 end
