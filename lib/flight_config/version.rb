6e092fddde41fbcea0f5d6fbe181ff5a201571b9
william <william.mccumstie@gmail.com>
2019-06-22 13:06:52 +0100

    Bump the version to 2.0.0

--- lib/flight_config/version.rb
+++ lib/flight_config/version.rb
@@ -25,5 +25,5 @@
 # https://github.com/openflighthpc/flight_config
 #==============================================================================
 module FlightConfig
-  VERSION = "0.1.1"
+  VERSION = "0.2.0"
 end
